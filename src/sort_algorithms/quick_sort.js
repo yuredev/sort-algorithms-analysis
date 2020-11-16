23597463f77c7f13c0ce8dcd09ea07f0e42710c8
alysonmiguel <alysonmiguel16@gmail.com>
2020-11-16 17:31:31 -0300

    Foi adicionado o retorno no quick sort

--- src/sort_algorithms/quick_sort.js
+++ src/sort_algorithms/quick_sort.js
@@ -1,23 +1,22 @@
 module.exports = function quickSort(array, inferior, superior) {
   const copy = [ ...array ];
+  let pivo_pos;
   if (inferior < superior) {
-    let pivo = copy[superior];
+    let pivo = copy[superior-1];
     let i, j;
     i = inferior - 1;
-    for (j = inferior; j <= superior - 1; j++) {
+    for (j = inferior; j < superior - 1; j++) {
       if (copy[j] <= pivo) {
         i++;
-        let aux = copy[j];
-        copy[j] = copy[i];
-        copy[i] = aux;
+        [copy[j],copy[i]] = [copy[i],copy[j]];
       }
     }
-    let aux = copy[superior];
-    copy[superior] = copy[i + 1];
-    copy[i + 1] = aux;
+    [copy[superior-1],copy[i+1]] = [copy[i+1],copy[superior-1]];
     pivo_pos = i + 1;
 
     quickSort(copy, inferior, pivo_pos - 1);
     quickSort(copy, pivo_pos + 1, superior);
+
+    return copy;
   }
 };
---
+++ src/test/test.js
@@ -0,0 +1,19 @@
+function teste() {
+    for(let i = 0; i < 100; i++);
+}
+
+console.time()
+teste();
+console.timeEnd();
+
+console.time()
+teste();
+console.timeEnd();
+
+console.time()
+teste();
+console.timeEnd();
+
+console.time()
+teste();
+console.timeEnd();
